44efb0b2ae694e2715b9ed433073403dc5e65448
Mateus <mateusrdgs@yahoo.com.br>
2015-10-14 23:39:16 -0300

    Commit 01 - Criado mainForm e cadEventoForm

---
+++ semanaTec/Forms/cadEventoForms.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace semanaTec.Forms
+{
+    partial class cadEventoForms
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nomeLbl = new System.Windows.Forms.Label();
+            this.localLbl = new System.Windows.Forms.Label();
+            this.dataLbl = new System.Windows.Forms.Label();
+            this.horaLbl = new System.Windows.Forms.Label();
+            this.tipoLbl = new System.Windows.Forms.Label();
+            this.duracaoLbl = new System.Windows.Forms.Label();
+            this.descricaoLbl = new System.Windows.Forms.Label();
+            this.codPLbl = new System.Windows.Forms.Label();
+            this.vagasLbl = new System.Windows.Forms.Label();
+            this.nomeTxt = new System.Windows.Forms.TextBox();
+            this.localTxt = new System.Windows.Forms.TextBox();
+            this.duracaoTxt = new System.Windows.Forms.TextBox();
+            this.codPTxt = new System.Windows.Forms.TextBox();
+            this.vagasTxt = new System.Windows.Forms.TextBox();
+            this.palestraRB = new System.Windows.Forms.RadioButton();
+            this.minicursoRB = new System.Windows.Forms.RadioButton();
+            this.descricaoTxt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // nomeLbl
+            // 
+            this.nomeLbl.AutoSize = true;
+            this.nomeLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nomeLbl.Location = new System.Drawing.Point(29, 26);
+            this.nomeLbl.Name = "nomeLbl";
+            this.nomeLbl.Size = new System.Drawing.Size(51, 20);
+            this.nomeLbl.TabIndex = 0;
+            this.nomeLbl.Text = "Nome";
+            // 
+            // localLbl
+            // 
+            this.localLbl.AutoSize = true;
+            this.localLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.localLbl.Location = new System.Drawing.Point(29, 59);
+            this.localLbl.Name = "localLbl";
+            this.localLbl.Size = new System.Drawing.Size(47, 20);
+            this.localLbl.TabIndex = 1;
+            this.localLbl.Text = "Local";
+            // 
+            // dataLbl
+            // 
+            this.dataLbl.AutoSize = true;
+            this.dataLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataLbl.Location = new System.Drawing.Point(312, 26);
+            this.dataLbl.Name = "dataLbl";
+            this.dataLbl.Size = new System.Drawing.Size(44, 20);
+            this.dataLbl.TabIndex = 2;
+            this.dataLbl.Text = "Data";
+            // 
+            // horaLbl
+            // 
+            this.horaLbl.AutoSize = true;
+            this.horaLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.horaLbl.Location = new System.Drawing.Point(312, 62);
+            this.horaLbl.Name = "horaLbl";
+            this.horaLbl.Size = new System.Drawing.Size(44, 20);
+            this.horaLbl.TabIndex = 3;
+            this.horaLbl.Text = "Hora";
+            // 
+            // tipoLbl
+            // 
+            this.tipoLbl.AutoSize = true;
+            this.tipoLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tipoLbl.Location = new System.Drawing.Point(29, 94);
+            this.tipoLbl.Name = "tipoLbl";
+            this.tipoLbl.Size = new System.Drawing.Size(39, 20);
+            this.tipoLbl.TabIndex = 4;
+            this.tipoLbl.Text = "Tipo";
+            // 
+            // duracaoLbl
+            // 
+            this.duracaoLbl.AutoSize = true;
+            this.duracaoLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.duracaoLbl.Location = new System.Drawing.Point(29, 143);
+            this.duracaoLbl.Name = "duracaoLbl";
+            this.duracaoLbl.Size = new System.Drawing.Size(70, 20);
+            this.duracaoLbl.TabIndex = 5;
+            this.duracaoLbl.Text = "Duração";
+            // 
+            // descricaoLbl
+            // 
+            this.descricaoLbl.AutoSize = true;
+            this.descricaoLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.descricaoLbl.Location = new System.Drawing.Point(29, 188);
+            this.descricaoLbl.Name = "descricaoLbl";
+            this.descricaoLbl.Size = new System.Drawing.Size(80, 20);
+            this.descricaoLbl.TabIndex = 6;
+            this.descricaoLbl.Text = "Descrição";
+            // 
+            // codPLbl
+            // 
+            this.codPLbl.AutoSize = true;
+            this.codPLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.codPLbl.Location = new System.Drawing.Point(174, 143);
+            this.codPLbl.Name = "codPLbl";
+            this.codPLbl.Size = new System.Drawing.Size(90, 40);
+            this.codPLbl.TabIndex = 7;
+            this.codPLbl.Text = "Código do\r\nPalestrante";
+            // 
+            // vagasLbl
+            // 
+            this.vagasLbl.AutoSize = true;
+            this.vagasLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.vagasLbl.Location = new System.Drawing.Point(362, 146);
+            this.vagasLbl.Name = "vagasLbl";
+            this.vagasLbl.Size = new System.Drawing.Size(76, 20);
+            this.vagasLbl.TabIndex = 8;
+            this.vagasLbl.Text = "Nº Vagas";
+            // 
+            // nomeTxt
+            // 
+            this.nomeTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nomeTxt.Location = new System.Drawing.Point(85, 23);
+            this.nomeTxt.Name = "nomeTxt";
+            this.nomeTxt.Size = new System.Drawing.Size(208, 26);
+            this.nomeTxt.TabIndex = 9;
+            // 
+            // localTxt
+            // 
+            this.localTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.localTxt.Location = new System.Drawing.Point(85, 56);
+            this.localTxt.Name = "localTxt";
+            this.localTxt.Size = new System.Drawing.Size(208, 26);
+            this.localTxt.TabIndex = 10;
+            // 
+            // duracaoTxt
+            // 
+            this.duracaoTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.duracaoTxt.Location = new System.Drawing.Point(105, 140);
+            this.duracaoTxt.Name = "duracaoTxt";
+            this.duracaoTxt.Size = new System.Drawing.Size(65, 26);
+            this.duracaoTxt.TabIndex = 11;
+            // 
+            // codPTxt
+            // 
+            this.codPTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.codPTxt.Location = new System.Drawing.Point(270, 143);
+            this.codPTxt.Name = "codPTxt";
+            this.codPTxt.Size = new System.Drawing.Size(86, 26);
+            this.codPTxt.TabIndex = 12;
+            // 
+            // vagasTxt
+            // 
+            this.vagasTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.vagasTxt.Location = new System.Drawing.Point(444, 143);
+            this.vagasTxt.Name = "vagasTxt";
+            this.vagasTxt.Size = new System.Drawing.Size(78, 26);
+            this.vagasTxt.TabIndex = 13;
+            // 
+            // palestraRB
+            // 
+            this.palestraRB.AutoSize = true;
+            this.palestraRB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.palestraRB.Location = new System.Drawing.Point(85, 92);
+            this.palestraRB.Name = "palestraRB";
+            this.palestraRB.Size = new System.Drawing.Size(85, 24);
+            this.palestraRB.TabIndex = 14;
+            this.palestraRB.TabStop = true;
+            this.palestraRB.Text = "Palestra";
+            this.palestraRB.UseVisualStyleBackColor = true;
+            // 
+            // minicursoRB
+            // 
+            this.minicursoRB.AutoSize = true;
+            this.minicursoRB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minicursoRB.Location = new System.Drawing.Point(200, 92);
+            this.minicursoRB.Name = "minicursoRB";
+            this.minicursoRB.Size = new System.Drawing.Size(94, 24);
+            this.minicursoRB.TabIndex = 15;
+            this.minicursoRB.TabStop = true;
+            this.minicursoRB.Text = "Minicurso";
+            this.minicursoRB.UseVisualStyleBackColor = true;
+            // 
+            // descricaoTxt
+            // 
+            this.descricaoTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.descricaoTxt.Location = new System.Drawing.Point(33, 211);
+            this.descricaoTxt.Multiline = true;
+            this.descricaoTxt.Name = "descricaoTxt";
+            this.descricaoTxt.Size = new System.Drawing.Size(489, 180);
+            this.descricaoTxt.TabIndex = 16;
+            // 
+            // cadEventoForms
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 412);
+            this.Controls.Add(this.descricaoTxt);
+            this.Controls.Add(this.minicursoRB);
+            this.Controls.Add(this.palestraRB);
+            this.Controls.Add(this.vagasTxt);
+            this.Controls.Add(this.codPTxt);
+            this.Controls.Add(this.duracaoTxt);
+            this.Controls.Add(this.localTxt);
+            this.Controls.Add(this.nomeTxt);
+            this.Controls.Add(this.vagasLbl);
+            this.Controls.Add(this.codPLbl);
+            this.Controls.Add(this.descricaoLbl);
+            this.Controls.Add(this.duracaoLbl);
+            this.Controls.Add(this.tipoLbl);
+            this.Controls.Add(this.horaLbl);
+            this.Controls.Add(this.dataLbl);
+            this.Controls.Add(this.localLbl);
+            this.Controls.Add(this.nomeLbl);
+            this.Name = "cadEventoForms";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "cadEventoForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nomeLbl;
+        private System.Windows.Forms.Label localLbl;
+        private System.Windows.Forms.Label dataLbl;
+        private System.Windows.Forms.Label horaLbl;
+        private System.Windows.Forms.Label tipoLbl;
+        private System.Windows.Forms.Label duracaoLbl;
+        private System.Windows.Forms.Label descricaoLbl;
+        private System.Windows.Forms.Label codPLbl;
+        private System.Windows.Forms.Label vagasLbl;
+        private System.Windows.Forms.TextBox nomeTxt;
+        private System.Windows.Forms.TextBox localTxt;
+        private System.Windows.Forms.TextBox duracaoTxt;
+        private System.Windows.Forms.TextBox codPTxt;
+        private System.Windows.Forms.TextBox vagasTxt;
+        private System.Windows.Forms.RadioButton palestraRB;
+        private System.Windows.Forms.RadioButton minicursoRB;
+        private System.Windows.Forms.TextBox descricaoTxt;
+    }
+}
---
+++ semanaTec/Forms/mainForm.cs
@@ -0,0 +1,40 @@
+﻿using semanaTec.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace semanaTec
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+        cadEventoForms cadEventoForm;
+
+        private void eventoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild != null)
+            {
+                this.ActiveMdiChild.Hide();
+            }
+            cadEventoForm = null;
+
+            if (cadEventoForm == null)
+            {
+                cadEventoForm = new cadEventoForms();
+            }
+
+            cadEventoForm.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            cadEventoForm.MdiParent = this;
+            cadEventoForm.Show();   
+        }
+    }
+}
